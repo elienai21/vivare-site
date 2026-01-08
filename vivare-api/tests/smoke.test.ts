856968acd870bdb79b9f06d0518e8fccf663013a
elienai <elienai@vivarestay.com>
2026-01-08 12:04:00 -0300

    update_08_01_2026

--- vivare-api/tests/smoke.test.ts
+++
@@ -1,7 +0,0 @@
-import request from 'supertest';
-// Simple test to check if the environment is working
-describe('Smoke Test', () => {
-    it('should pass', () => {
-        expect(1 + 1).toBe(2);
-    });
-});
